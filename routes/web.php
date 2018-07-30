74f6d7d362b89a119103b8c8fc2b45f1233c54c2
joaohamerski <joaohamerski@hotmail.com>
2018-07-30 16:09:56 -0300

    alterações nas rotas

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,15 @@
 <?php
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'IndexController@index');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::prefix('cursos')->name('courses.')->group(function () {
+	Route::get('/', 'CourseController@index');
     Route::post('/', 'CourseController@store')->name('store')->middleware('admin');
 });
+
+Route::get('eventos', 'EventController@index');
+Route::get('noticias', 'NewsController@index');
