19dacdc8e90d1720cf5b2993c161c4abd2c8ee0f
joaohamerski <joaohamerski@hotmail.com>
2018-07-30 16:08:36 -0300

    assets

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,22 +1,9 @@
+import jQuery from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap';
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+window.$ = window.jQuery = jQuery;
+window.Popper = Popper.default;
 
-require('./bootstrap');
+require('./partials/all');
 
-window.Vue = require('vue');
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
-const app = new Vue({
-    el: '#app'
-});
