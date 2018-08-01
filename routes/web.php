227d2c7053a07ed4e61f3da8f94333dccbaa40a7
joaohamerski <joaohamerski@hotmail.com>
2018-07-31 22:30:35 -0300

    adicionada rotas para testar as views

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::prefix('cursos')->name('courses.')->group(function () {
     Route::get('/', 'CourseController@index');
+    Route::get('/show', 'CourseController@show');
     Route::post('/', 'CourseController@store')->name('store')->middleware('admin');
 });
 
