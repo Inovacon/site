7e5607a675e81d0a36d8ee9ebf04235c7f2ee4d2
joaohamerski <joaohamerski@hotmail.com>
2018-08-16 17:02:27 -0300

    biblioteca chart.js adicionada

--- resources/assets/js/dependencies.js
+++ resources/assets/js/dependencies.js
@@ -4,9 +4,11 @@ import jQuery from 'jquery';
 import Popper from 'popper.js';
 import 'bootstrap';
 import toastr from 'toastr';
+import chart from 'chart.js';
 
 window.Vue = Vue;
 window.axios = axios;
 window.$ = window.jQuery = jQuery;
 window.Popper = Popper.default;
 window.toastr = toastr;
+// window.chartjs = chart;
