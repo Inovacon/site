32bea6dd38fa827e58550ebf736f6bfcb59719fc
joaohamerski <joaohamerski@hotmail.com>
2018-08-18 21:28:47 -0300

    apenas consertos

--- resources/views/layouts/auth/login-form.blade.php
+++ resources/views/layouts/auth/login-form.blade.php
@@ -1,39 +1,43 @@
-<div class="form-group">
-    <div class="form-md-group">
-        <input  name="email" 
-                id="email" 
-                value="{{ old('email') }}" 
-                class="form-control md-input {{ old('email') ? 'active' : '' }}" 
-                type="email"/>
+<form action="{{ route('login') }}" method="POST">
+    @csrf
+    
+    <div class="form-group">
+        <div class="form-md-group">
+            <input  name="email" 
+                    id="email" 
+                    value="{{ old('email') }}" 
+                    class="form-control md-input {{ old('email') ? 'active' : '' }}" 
+                    type="email"/>
 
-        <label class="md-label" for="email">
-            <i class="fas fa-envelope fa-md mr-1"></i> E-mail
-        </label>
+            <label class="md-label" for="email">
+                <i class="fas fa-envelope fa-md mr-1"></i> E-mail
+            </label>
+        </div>
     </div>
-</div>
 
-<div class="form-group">
-    <div class="form-md-group">
-        <input  name="password" 
-                id="password" 
-                class="form-control md-input"  
-                type="password"/>
+    <div class="form-group">
+        <div class="form-md-group">
+            <input  name="password" 
+                    id="password" 
+                    class="form-control md-input"  
+                    type="password"/>
 
-        <label class="md-label" for="password">
-            <i class="fas fa-lock fa-md mr-1"></i> Senha
-        </label>
+            <label class="md-label" for="password">
+                <i class="fas fa-lock fa-md mr-1"></i> Senha
+            </label>
+        </div>
     </div>
-</div>
 
-<div class="form-group custom-control custom-checkbox">
-    <input  name="remember"
-            id="rememberCheck" {{ old('remember') ? 'checked' : '' }}
-            class="custom-control-input pointer" 
-            type="checkbox"/>
+    <div class="form-group custom-control custom-checkbox">
+        <input  name="remember"
+                id="rememberCheck" {{ old('remember') ? 'checked' : '' }}
+                class="custom-control-input pointer" 
+                type="checkbox"/>
 
-    <label class="custom-control-label text-gray-dark" for="rememberCheck">Manter-se conectado</label>
-</div>
+        <label class="custom-control-label text-gray-dark" for="rememberCheck">Manter-se conectado</label>
+    </div>
 
-<div class="form-group mb-0">
-    <button id="btnLogin" type="submit" class="btn btn-primary">Entrar</button>
-</div>
+    <div class="form-group mb-0">
+        <button id="btnLogin" type="submit" class="btn btn-primary">Entrar</button>
+    </div>
+</form>
--- resources/views/layouts/auth/register-form.blade.php
+++ resources/views/layouts/auth/register-form.blade.php
@@ -1,43 +1,37 @@
-<ul class="nav nav-pills nav-fill mb-3" id="pills-tab" role="tablist">
-    <li class="nav-item">
-        <a  class="nav-link active" 
-            id="pills-physical-person-tab" 
-            data-toggle="pill" 
-            href="#pills-physical-person" 
-            role="tab" 
-            aria-controls="pills-physical-person" 
-            aria-selected="true"><i class="fas fa-portrait  mr-1"></i> Pessoa física</a>
-    </li>
+<form action="{{ route('register') }}" method="POST">
+    <ul class="nav nav-pills nav-fill mb-3" id="pills-tab" role="tablist">
+        <li class="nav-item">
+            <a  class="nav-link active" 
+                id="pills-physical-person-tab" 
+                data-toggle="pill" 
+                href="#pills-physical-person" 
+                role="tab" 
+                aria-controls="pills-physical-person" 
+                aria-selected="true"><i class="fas fa-portrait  mr-1"></i> Pessoa física</a>
+        </li>
 
-    <li class="nav-item">
-        <a  class="nav-link" 
-            id="pills-legal-person-tab" 
-            data-toggle="pill" 
-            href="#pills-legal-person" 
-            role="tab" 
-            aria-controls="pills-legal-person" 
-            aria-selected="false"><i class="fas fa-building  mr-1"></i> Pessoa jurídica</a>
-    </li>
-</ul>
+        <li class="nav-item">
+            <a  class="nav-link" 
+                id="pills-legal-person-tab" 
+                data-toggle="pill" 
+                href="#pills-legal-person" 
+                role="tab" 
+                aria-controls="pills-legal-person" 
+                aria-selected="false"><i class="fas fa-building  mr-1"></i> Pessoa jurídica</a>
+        </li>
+    </ul>
 
-<div class="tab-content" id="pills-tabContent">
-    <div class="tab-pane fade show active" id="pills-physical-person" role="tabpanel" aria-labelledby="pills-physical-person-tab">
-        @include('layouts.auth._physical-person-form')
+    <div class="tab-content" id="pills-tabContent">
+        <div class="tab-pane fade show active" id="pills-physical-person" role="tabpanel" aria-labelledby="pills-physical-person-tab">
+            @include('layouts.auth._physical-person-form')
+        </div>
+
+        <div class="tab-pane fade" id="pills-legal-person" role="tabpanel" aria-labelledby="pills-legal-person-tab">
+            @include('layouts.auth._legal-person-form')
+        </div>
     </div>
 
-    <div class="tab-pane fade" id="pills-legal-person" role="tabpanel" aria-labelledby="pills-legal-person-tab">
-        @include('layouts.auth._legal-person-form')
+    <div class="form-group mb-0">
+        <button class="btn btn-success" type="submit">Cadastrar-se</button>
     </div>
-</div>
-
-<div class="form-group mb-0">
-    <button class="btn btn-success" type="submit">Cadastrar-se</button>
-</div>
-
-@push('scripts')
-    <script>
-
-
-
-    </script>
-@endpush
+</form>
