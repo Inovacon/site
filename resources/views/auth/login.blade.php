81480d7740fd41943ccb0c2a475d3b296ab4e043
joaohamerski <joaohamerski@hotmail.com>
2018-08-18 21:12:25 -0300

    modificações na tela de login e cadastro

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,22 +1,25 @@
 @extends('layouts.app')
 
-@section('content')
-<div class="container">
-  <div class="row justify-content-center">
-    <div class="col-md-8">
-      <div class="card">
-        <div class="card-header">
-          <h5 class="font-weight-600">          
-            <i class="fas fa-user fa-fw mr-2 fa-lg"></i>
-            Entre com sua conta
-          </h5>
-        </div>
+@section('title', 'Entre | Inovacon')
 
-        <div class="card-body">
-          @include('layouts.login-form')
+@section('content')
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header bg-primary">
+            <h5 class="font-weight-600 text-white mb-0">          
+              ENTRE COM SUA CONTA
+            </h5>
+          </div>  
+          
+          <hr class="my-0">
+
+          <div class="card-body">
+            @include('layouts.auth.login-form')
+          </div>
         </div>
       </div>
     </div>
   </div>
-</div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,22 +1,26 @@
 @extends('layouts.app')
 
-@section('content')
-<div class="container">
-  <div class="row justify-content-center">
-    <div class="col-md-8">
-      <div class="card">
-        <div class="card-header">
-          <h5 class="font-weight-600">          
-            <i class="fas fa-id-card fa-fw mr-2 fa-lg"></i>
-            Cadastre seus dados
-          </h5>
-        </div>
+@section('title', 'Cadastre-se | Inovacon')
 
-        <div class="card-body">
-          @include('layouts.register-form')
+@section('content')
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header bg-primary">
+            <h5 class="font-weight-600 text-white mb-0">          
+              CADASTRE SEUS DADOS
+            </h5>
+
+          </div>
+          
+          <hr class="mt-0">
+
+          <div class="card-body">
+            @include('layouts.auth.register-form')
+          </div>
         </div>
       </div>
     </div>
   </div>
-</div>
 @endsection
