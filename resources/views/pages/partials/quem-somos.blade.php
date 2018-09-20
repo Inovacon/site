95293b947cb5eb26cdb669bd18a3b053341a0f23
pedrothome1 <pedrothome2@outlook.com>
2018-09-20 13:34:46 -0300

    Página inicial - Melhoria da responsividade da seção "Quem somos"

--- resources/views/pages/partials/quem-somos.blade.php
+++ resources/views/pages/partials/quem-somos.blade.php
@@ -11,14 +11,14 @@
 <div style="background-color: rgba(0, 0, 0, .05);">
 	<div class="container">
 		<div class="row py-5">
-			<div class="col text-dark">
-				<h2 class="font-weight-bold mb-3">Quem Somos</h2>
+			<div class="col-md-6 text-dark">
+				<h2 class="font-weight-bold mb-3 text-center text-md-left">Quem Somos</h2>
 
 				<p class="text-gray-dark text-justify" style="text-indent: 3rem; line-height: 1.7rem;">A Inovacon Empresa Júnior é uma associação civil, sem fins lucrativos, constituída exclusivamente por alunos da graduação, dos cursos de administração, ciências contábeis e análise e desenvolvimento de sistemas que desenvolve estudos para empresas, entidades e sociedade em geral, nas suas áreas de atuação, sob a supervisão de professores.</p>
 				<p class="text-gray-dark text-justify" style="text-indent: 3rem; line-height: 1.7rem;"></p>
 			</div>
 
-			<div class="col">
+			<div class="col-md-6">
 				<img class="img-fluid" src="{{ asset('images/reuniao-mini.jpg') }}" alt="">
 			</div>
 		</div>
