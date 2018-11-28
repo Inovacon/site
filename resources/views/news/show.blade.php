a0a3f6dbc29321e4c157722de7165ad5cf64e3f9
pedrothome1 <pedrothome2@outlook.com>
2018-11-28 11:03:43 -0200

    Ajustes

--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -75,7 +75,7 @@
 						<div class="align-self-end">
                             <social-sharing
                                 title="{{ $noticia->title }}"
-                                description="{{ str_limit(strip_tags($noticia->body), 100) }}"
+                                description="{{ str_limit(html_entity_decode(strip_tags($noticia->body)), 100) }}"
                                 quote="{{ $noticia->title }}"
                                 inline-template>
 
